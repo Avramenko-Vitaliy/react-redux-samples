56aa7d6c413777850e96f5b5b0f199f897c94976
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-05-18 14:23:15 +0300

    fix example

--- immutable-js/index.js
+++ immutable-js/index.js
@@ -3,14 +3,14 @@ const List = require("immutable").List;
 
 (function () {
     const list = fromJS([
-        { name: 'Jora', age: 12 },
-        { name: 'Liza', age: 32 },
-        { name: 'Petya', age: 22 },
-        { name: 'Nick', age: 12 },
-        { name: 'John', age: 33 },
-        { name: 'Ted', age: 22 },
-        { name: 'Pit', age: 45 },
-        { name: 'Elizabet', age: 12 },
+        {name: 'Jora', age: 12},
+        {name: 'Liza', age: 32},
+        {name: 'Petya', age: 22},
+        {name: 'Nick', age: 12},
+        {name: 'John', age: 33},
+        {name: 'Ted', age: 22},
+        {name: 'Pit', age: 45},
+        {name: 'Elizabet', age: 12},
     ]);
 
     const grouped = list.groupBy(item => item.get('age'));
@@ -21,14 +21,14 @@ const List = require("immutable").List;
 
 (function () {
     const list = fromJS([
-        { name: 'Jora', interests: ['Sport', 'Games', 'Pets', 'Traveling'] },
-        { name: 'Liza', interests: ['Films', 'Music', 'IT', 'React'] },
-        { name: 'Petya', interests: ['Redux', 'Fashion', 'Natural', 'Coffee'] },
-        { name: 'Nick', interests: ['Games', 'Films', 'Pets', 'Sport'] },
-        { name: 'John', interests: ['Traveling', 'Sport', 'Music', 'Natural'] },
-        { name: 'Ted', interests: ['TV', 'IT', 'Coffee', 'React'] },
-        { name: 'Pit', interests: ['Redux', 'Sport'] },
-        { name: 'Elizabet', interests: [] },
+        {name: 'Jora', interests: ['Sport', 'Games', 'Pets', 'Traveling']},
+        {name: 'Liza', interests: ['Films', 'Music', 'IT', 'React']},
+        {name: 'Petya', interests: ['Redux', 'Fashion', 'Natural', 'Coffee']},
+        {name: 'Nick', interests: ['Games', 'Films', 'Pets', 'Sport']},
+        {name: 'John', interests: ['Traveling', 'Sport', 'Music', 'Natural']},
+        {name: 'Ted', interests: ['TV', 'IT', 'Coffee', 'React']},
+        {name: 'Pit', interests: ['Redux', 'Sport']},
+        {name: 'Elizabet', interests: []},
     ]);
 
     const countLikeSport = list.flatMap(item => item.get('interests'))
@@ -39,24 +39,22 @@ const List = require("immutable").List;
 })();
 
 (function () {
-    const list = fromJS([
-        { name: 'Jora', interests: ['Sport', 'Games', 'Pets', 'Traveling'] },
-        { name: 'Liza', interests: ['Films', 'Music', 'IT', 'React'] },
-        { name: 'Petya', interests: ['Redux', 'Fashion', 'Natural', 'Coffee'] },
-        { name: 'Nick', interests: ['Games', 'Films', 'Pets', 'Sport'] },
-        { name: 'John', interests: ['Traveling', 'Sport', 'Music', 'Natural'] },
-        { name: 'Ted', interests: ['TV', 'IT', 'Coffee', 'React'] },
-        { name: 'Pit', interests: ['Redux', 'Sport'] },
-        { name: 'Elizabet' },
-    ]);
-
-    const updatedInterests = list.updateIn(['interests'], List(), item => {
-        const index = item.indexOf('Sport');
-        if (index >= 0) {
-            return item.set(index, 'Programming');
-        }
-        return item;
+    const entity = fromJS({
+        people: [
+            {name: 'Jora', interests: ['Sport', 'Games', 'Pets', 'Traveling']},
+            {name: 'Liza', interests: ['Films', 'Music', 'IT', 'React']},
+            {name: 'Petya', interests: ['Redux', 'Fashion', 'Natural', 'Coffee']},
+            {name: 'Nick', interests: ['Games', 'Films', 'Pets', 'Sport']},
+            {name: 'John', interests: ['Traveling', 'Sport', 'Music', 'Natural']},
+            {name: 'Ted', interests: ['TV', 'IT', 'Coffee', 'React']},
+            {name: 'Pit', interests: ['Redux', 'Sport']},
+            {name: 'Elizabet'},
+        ]
     });
 
-    console.log('>>>>>>>>>>>> Replaced interest "Sport" at "Programming": ', updatedInterests.toJS());
+    const updatedInterests = entity.update('people', List(), list =>
+        list.filterNot(item => item.get('interests', List()).contains('Sport'))
+    );
+
+    console.log('>>>>>>>>>>>> Update people list:\n', JSON.stringify(updatedInterests.toJS()));
 })();
